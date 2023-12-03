7efd6885e9c2e713b7915e5a1182a487e2383a11
Oskar Brzeski <oskar.brzeski.github@hotmail.com>
2023-12-03 14:57:11 +0000

    change imports

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-import * as day1 from "./2023/code/day1.js";
-import * as day2 from "./2023/code/day2.js";
-import * as day3 from "./2023/code/day3.js";
+import { solution as day1 } from "./2023/code/day1.js";
+import { solution as day2 } from "./2023/code/day2.js";
+import { solution as day3 } from "./2023/code/day3.js";
 
 function main() {
-    day1.solution();
-    day2.solution();
-    day3.solution();
+    day1();
+    day2();
+    day3();
 }
 
 main();

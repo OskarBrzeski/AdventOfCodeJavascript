71088973e0d334dda5df928ff00701d5601a1d80
Oskar Brzeski <oskar.brzeski.github@hotmail.com>
2023-12-04 13:21:20 +0000

    complete day 4

---
+++ 2023/code/day4.js
@@ -0,0 +1,82 @@
+import * as fs from "fs";
+
+export function solution() {
+    fs.readFile("2023/data/day4.txt", "utf8", (err, data) => {
+        if (err) throw err;
+        parts(data);
+    });
+}
+
+function parts(fileData) {
+    const lines = fileData.split("\n");
+    console.log(`Day4: ${part1(lines)} | ${part2(lines)}`)
+}
+
+function part1(lines) {
+    let result = 0;
+    
+    for (let line of lines) {
+        let amount = matchingAmount(...splitNumbers(line));
+        if (amount === 0) {continue;}
+        result += Math.pow(2, amount-1);
+    }
+
+    return result;
+}
+
+function part2(lines) {
+    let result = 0;
+
+    const cards = [];
+    for (let i = 0; i < lines.length; i++) {
+        cards.push(1);
+    }
+
+    for (let i = 0; i < lines.length; i++) {
+        let amount = matchingAmount(...splitNumbers(lines[i]));
+
+        for (let j = 1; j <= amount; j++) {
+            if (cards[i] === undefined) {break;}
+            cards[i + j] += cards[i];
+        }
+
+        result += cards[i]
+    }
+    
+    return result;
+}
+
+function splitNumbers(line) {
+    const pastColon = line.slice(10, line.length);
+    const [winningLine, mineLine] = pastColon.split(" | ");
+
+    const winning = [];
+    const mine = [];
+
+    for (let num of winningLine.split(" ")) {
+        if (num === "") {continue;}
+        winning.push(Number(num));
+    }
+
+    for (let num of mineLine.split(" ")) {
+        if (num === "") {continue;}
+        mine.push(Number(num));
+    }
+
+    return [winning, mine];
+}
+
+function matchingAmount(winning, mine) {
+    let count = 0;
+
+    for (let win of winning) {
+        for (let num of mine) {
+            if (num === win) {
+                count++;
+                break;
+            }
+        }
+    }
+
+    return count;
+}
--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import { solution as day1 } from "./2023/code/day1.js";
 import { solution as day2 } from "./2023/code/day2.js";
 import { solution as day3 } from "./2023/code/day3.js";
+import { solution as day4 } from "./2023/code/day4.js";
 
 function main() {
     day1();
     day2();
     day3();
+    day4();
 }
 
 main();

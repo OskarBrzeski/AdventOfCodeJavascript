8dc0d8d00994e8c14f66c36666b23881c89be1a3
Oskar Brzeski <oskar.brzeski.github@hotmail.com>
2023-12-21 00:11:25 +0000

    complete day 9

---
+++ 2023/code/day9.js
@@ -0,0 +1,59 @@
+import * as fs from "fs";
+
+export function solution() {
+    fs.readFile("2023/data/day9.txt", "utf8", (err, data) => {
+        if (err) throw err;
+        parts(data);
+    });
+}
+
+function parts(fileData) {
+    const lines = fileData.split("\n");
+    console.log(`Day9: ${part1(lines)} | ${part2(lines)}`)
+}
+
+function part1(lines) {
+    let result = 0;
+    
+    for (let line of lines) {
+        result += findNext(line.split(" ").map(Number));
+    }
+
+    return result;
+}
+
+function part2(lines) {
+    let result = 0;
+
+    for (let line of lines) {
+        result += findPrevious(line.split(" ").map(Number));
+    }
+
+    return result;
+}
+
+function findNext(sequence) {
+    let newSequence = [];
+    for (let i = 0; i < sequence.length-1; i++) {
+        newSequence.push(sequence[i+1] - sequence[i]);
+    }
+
+    if (newSequence[0] === newSequence[1] && newSequence[0] === newSequence[2]) {
+        return sequence[sequence.length-1] + newSequence[0];
+    }
+
+    return sequence[sequence.length-1] + findNext(newSequence);
+}
+
+function findPrevious(sequence) {
+    let newSequence = [];
+    for (let i = 0; i < sequence.length-1; i++) {
+        newSequence.push(sequence[i+1] - sequence[i]);
+    }
+
+    if (newSequence[0] === newSequence[1] && newSequence[0] === newSequence[2]) {
+        return sequence[0] - newSequence[0];
+    }
+
+    return sequence[0] - findPrevious(newSequence);
+}
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import { solution as day5 } from "./2023/code/day5.js";
 import { solution as day6 } from "./2023/code/day6.js";
 import { solution as day7 } from "./2023/code/day7.js";
 import { solution as day8 } from "./2023/code/day8.js";
+import { solution as day9 } from "./2023/code/day9.js";
 
 function main() {
     day1();
@@ -15,7 +16,8 @@ function main() {
     // day5();
     day6();
     day7();
-    day8();
+    // day8();
+    day9();
 }
 
 main();
